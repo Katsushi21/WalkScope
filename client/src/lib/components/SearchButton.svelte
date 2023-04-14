2f2f2f06880fa615534dd308c8aef16dec174291
Katsushi21 <sheoaspendie@gmail.com>
2023-04-15 00:50:44 +0900

    update: 　フロントエンドコンポーネント実装開始

---
+++ client/src/lib/components/SearchButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import IconButton from '@smui/icon-button';
+
+	let clicked = 0;
+</script>
+
+<div style="display: flex; align-items: center;">
+	<IconButton class="material-icons" on:click={() => clicked++}>search</IconButton>
+</div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  export let title = 'Webページのタイトル';
+</script>
+
+<style>
+  /* ページ全体のスタイル */
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+  }
+
+  /* 上部の検索バー */
+  #kensaku {
+    height: 100px;
+    background-color: #eee;
+    text-align: center;
+    font-size: 24px;
+    line-height: 100px;
+  }
+
+  /* 地図表示エリア */
+  #map {
+    height: calc(100vh - 200px);
+    background-color: #ccc;
+  }
+
+  /* 下部の保存ボタン */
+  #hozon {
+    height: 100px;
+    background-color: #eee;
+    text-align: center;
+    font-size: 24px;
+    line-height: 100px;
+  }
+
+  /* 全体レイアウト */
+  #container {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 100px 1fr 100px;
+    grid-template-areas: "kensaku" "map" "hozon";
+  }
+
+  /* レスポンシブ対応 */
+  @media screen and (max-width: 768px) {
+    #container {
+      grid-template-rows: 50px 1fr 50px;
+    }
+    #kensaku, #hozon {
+      font-size: 16px;
+      line-height: 50px;
+    }
+  }
+</style>
+
+<div id="container">
+  <div id="kensaku">kensaku</div>
+  <div id="map">map</div>
+  <div id="hozon">hozon</div>
+</div>
