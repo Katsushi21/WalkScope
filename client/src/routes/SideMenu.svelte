b0db932085a6a800dba914d3b866b296cd84b7ee
Katsushi21 <sheoaspendie@gmail.com>
2023-04-23 19:56:01 +0900

    update: tailwind適用

---
+++ client/src/routes/SideMenu.svelte
@@ -0,0 +1,11 @@
+<ul class="py-4">
+	<li class="mb-2">
+		<a href="#" class="block p-2 text-gray-800 hover:bg-gray-200 rounded"> メニュー項目 1 </a>
+	</li>
+	<li class="mb-2">
+		<a href="#" class="block p-2 text-gray-800 hover:bg-gray-200 rounded"> メニュー項目 2 </a>
+	</li>
+	<li class="mb-2">
+		<a href="#" class="block p-2 text-gray-800 hover:bg-gray-200 rounded"> メニュー項目 3 </a>
+	</li>
+</ul>
