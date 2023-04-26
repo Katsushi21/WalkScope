cd32652ca6e4631e33ffe6fac2b441916b7b6d4c
Katsushi21 <sheoaspendie@gmail.com>
2023-04-26 23:22:04 +0900

    update: Conditionコンポーネントを作成

--- client/src/routes/ConditionList.svelte
+++ client/src/routes/ConditionList.svelte
@@ -1,3 +1,16 @@
-<script>
-	let condition = [];
+<script lang="ts">
+	import Condition from './Condition.svelte';
+
+	type Condition = {
+		condition: string[];
+	};
+	type Conditions = {
+		city: string;
+		conditions: Condition[];
+	};
+	let conditions: Conditions = [];
 </script>
+
+{#each conditions as condition}
+	<Condition {condition.city} {condition.Condition} />
+{/each}
