87ae5d6d47f7b1ae6d40be74b9b41224348050bf
Katsushi21 <sheoaspendie@gmail.com>
2023-05-04 16:19:13 +0900

    update:condition削除実装

---
+++ client/src/routes/components/AddConditionButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import IconButton from '@smui/icon-button';
+</script>
+
+<div style="display: flex; align-items: center;">
+	<IconButton class="material-icons" >add</IconButton>
+</div>
