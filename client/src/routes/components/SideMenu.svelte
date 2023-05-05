71393e9b56fd7e44105d12e9a5ce99589145565c
Katsushi21 <sheoaspendie@gmail.com>
2023-05-05 17:09:19 +0900

    update: サイドメニュー構成変更・設定ボタン追加

--- client/src/routes/components/SideMenu.svelte
+++ client/src/routes/components/SideMenu.svelte
@@ -3,11 +3,22 @@
 	import Setting from './Setting.svelte';
 </script>
 
-<div class=" bg-gray-500">
-	<div class="h-9/10">
-		<ConditionList />
+<div class="drawer drawer-mobile">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col items-center justify-center">
+		<label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 	</div>
-	<div class="h-1/10">
-		<Setting />
+	<div class="drawer-side">
+		<label for="my-drawer-2" class="drawer-overlay" />
+		<ul class="menu p-4 w-80 bg-base-100 text-base-content">
+			<div class="h-9/10">
+				<ConditionList />
+			</div>
+			<div class="divider" />
+
+			<div class="fixed bottom-0">
+				<Setting />
+			</div>
+		</ul>
 	</div>
 </div>
