24918ebb99ff56bff5da5ede939f23797875b0e8
Katsushi21 <sheoaspendie@gmail.com>
2023-05-20 14:06:28 +0900

    udpate: 条件追加ボタンの実装

--- client/src/routes/components/AddConditionButton.svelte
+++ client/src/routes/components/AddConditionButton.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
-	import IconButton from '@smui/icon-button';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	const addCondition = () => dispatch('addCondition');
 </script>
 
-<div style="display: flex; align-items: center;">
-	<IconButton class="material-icons" >add</IconButton>
-</div>
+<button class="btn btn-circle btn-outline" on:click={addCondition}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-6 h-6"
+	>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+	</svg>
+</button>
--- client/src/routes/components/ConditionList.svelte
+++ client/src/routes/components/ConditionList.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import CityCondition from './CityCondition.svelte';
+	import ScopeCondition from './ScopeCondition.svelte';
 	import Condition from './Condition.svelte';
+	import AddConditionButton from './AddConditionButton.svelte';
 
 	type Conditions = {
 		city: string;
@@ -8,16 +10,23 @@
 		scope: number;
 	};
 
-	let conditions: Conditions = { city: '', condition: ['0', '1', '2'], scope: 100 };
+	let conditions: Conditions = { city: 'yokohama', condition: ['0', '1', '2'], scope: 100 };
 
 	const deleteCondition = (index: number): void => {
 		conditions.condition.splice(index, 1);
 		conditions = conditions;
 	};
+	const addCondition = (): void => {
+		conditions.condition.push('');
+		conditions = conditions;
+	};
 </script>
 
 <CityCondition city={conditions.city} />
-
+<ScopeCondition scope={conditions.scope} />
 {#each conditions.condition as value, index}
 	<Condition {index} {value} on:deleteCondition={() => deleteCondition(index)} />
 {/each}
+{#if conditions.condition.length < 5}
+	<AddConditionButton on:addCondition={addCondition} />
+{/if}
