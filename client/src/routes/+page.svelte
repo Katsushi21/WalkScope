aa44e9aac406b436e70b696e0285fc01e19301c8
高野 且次 <katsushi@takanonoMacBook-Air.local>
2023-06-03 13:21:53 +0900

    udpate: メニュー開閉ボタンを左上に固定

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -15,7 +15,24 @@
 	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content flex flex-col items-center justify-center">
 		<Map />
-		<label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+		<label
+			for="my-drawer-2"
+			class="btn btn-outline btn-primary drawer-button absolute top-4 left-4 lg:hidden"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+				/>
+			</svg>
+		</label>
 	</div>
 	<div class="drawer-side">
 		<label for="my-drawer-2" class="drawer-overlay" />
